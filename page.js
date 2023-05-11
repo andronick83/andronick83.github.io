3f68098f9ca5739b14274a6cfcf8a69282b8b7df
ÅηDrσnick <andronick.mail@gmail.com>
2023-05-11 14:37:09 +0300

    Create page.js

---
+++ page.js
@@ -0,0 +1,20 @@
+var page=function(){};
+page.getCookie=(name,def=undefined)=>{var matches=document.cookie.match(new RegExp("(?:^|; )"+name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g,'\\$1')+"=([^;]*)"));return matches?decodeURIComponent(matches[1]):def};
+page.invert=()=>{page.dark=!page.dark;document.cookie="dark="+(+page.dark);page.dark?$('html').addClass('dark'):$('html').removeClass('dark');return false};
+page.dark=!parseInt(page.getCookie('dark',window.matchMedia&&window.matchMedia('(prefers-color-scheme:dark)').matches?1:0));
+page.invert();
+$(function($){
+	//$('html').addClass('ease');
+	//var navi=$('<div class=navi>').append($('<a class=hljs-string>').attr('href','https://github.com/andronick83/jquery.json-viewer-callback/').append('<b>JVC JSON-viewer</b>'));
+	var navi=$('<div class=navi>').append($('<a class=hljs-string>').attr('href','https://supervisor.parsing.run/').append('<b>SUPERVISOR-WEB</b>'));
+	var menu=$('<span class=fright>');
+	menu.append('<span class="ico ico-sun" onclick=page.invert(true)></span>');
+	navi.append(menu).prependTo('body');
+	setTimeout(()=>{
+		$.ajax('https://api.cdnjs.com/libraries/highlight.js?fields=assets').done(function(d){
+		var files=d.assets[0].files;
+		var styles=[$("<option/>",{text:'jvc-default',selected:'selected'})],s;
+		for(i in files){s=files[i].match(/^styles\/(.+)\.min\.css$/);if(s)styles.push($("<option/>",{text:s[1]}))}
+		var select=$('<select>');
+		select.append(styles).prependTo(menu).on('change',function(a,b,c){var s=select.val();JVC.setStyle(select.val())})})},0);
+});
