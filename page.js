250c4f70c59663cf76b997f5ce06b9f2193517ee
andronick83 <andronick.mail@gmail.com>
2023-05-25 21:27:44 +0300

    JavaScript Example Page
    JavaScript Example Page

--- page.js
+++ page.js
@@ -7,47 +7,31 @@
 
 "use strict";
 
-var page={};
+var page=page||{};
 page.getCookie=(name,def=null)=>{var matches=document.cookie.match(new RegExp("(?:^|; )"+
 	name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g,'\\$1')+"=([^;]*)"));return matches?decodeURIComponent(matches[1]):def};
-page.setCookie=(name,val=null)=>{document.cookie=name+"="+val+';SameSite=Lax'}
-page.dark=()=>{if(page._dark===null)page._dark=!parseInt(page.getCookie('dark',window.matchMedia&&
+page.setCookie=(name,val=null)=>{document.cookie=name+"="+val+';SameSite=Lax'};
+page.dark=(d=null)=>{if(!('_dark'in page))page._dark=null;if(page._dark===null)page._dark=!parseInt(page.getCookie('dark',window.matchMedia&&
 	window.matchMedia('(prefers-color-scheme:dark)').matches?0:1));page._dark=!page._dark;page.setCookie('dark',+page._dark);
 	var r=document.querySelector(':root');page._dark?r.classList.add('dark'):r.classList.remove('dark');return false};
-page._dark=null;page.dark();
+page.dark();
 
-var findUnique=(str)=>{let u="";for(let i=0;i<str.length;i++){if(u.includes(str[i])===false){u+=str[i]}}return u}
+// FontLoader
+var fontsDone=['Source Code Pro','monospace'];
+var JVC_setFontn=(n,cb=null)=>{//document.fonts.check('14px "Source Code Pro"');
+	var logger=logging.getLogger('JVC_setFontn');
+	var doneFont=function(){if(!fontsDone.includes(n))fontsDone.push(n);JVC.setFontFamily(n);cb&&cb(n)}
+	if(n=='monospace'){return doneFont()}if(n=='jvc-default'){n='"Source Code Pro",monospace';return doneFont()}
+	if(fontsDone.includes(n))return doneFont();
+	WebFont.load({classes:false,timeout:2000,google:{families:[n]},
+		fontloading:()=>{logger.debug('fontloading:',n)},
+		fontactive:()=>{logger.debug('fontactive:',n);doneFont()},
+		fontinactive:(...args)=>{logger.warning('fontinactive:',n,[...args]);doneFont()}})};
 
-var fontLoaderNode=()=>{var n=document.querySelector('.font-loader');if(n)return;
-	n=document.createElement('span');n.classList.add('jvc','font-loader');document.querySelector('body').append(n)};
-var fontLoaderSetFont=(n)=>{document.querySelector(":root").style.setProperty('--font-loader','"'+n+'"')};
-var fontLoaderSetText=()=>{var n=document.querySelector('.font-loader'),b=document.querySelector('body'),t=findUnique(b.innerText);n.innerText=t};
-var fontLoaderWait=(n)=>{var find=0,wait=0;var fontWait=(n)=>{
-		document.fonts.forEach((a,b,c)=>{
-			if(a.family!=n&&a.family!='"'+n+'"')return;
-			//console.log('fontWait',n,[a.family,b.family],c.status);
-			find=1;fontLoad(a,b,n)});
-		//console.log('fontWait',find?'find':'not-find');
-		if(find)return;
-		wait+=1;if(wait>200)return console.error('fontWait','timeout');setTimeout(fontWait,20,n)};
-	setTimeout(fontWait,20,n)};
-async function fontLoad(a,b,n){await a.load();await b.load();
-	//console.log('fontLoad',n,document.fonts.check("14px "+n),[a.status,b.status]);
-	setTimeout(JVC.setFontFamily,100,'"'+n+'"')}
-var JVC_setFontn=(n)=>{
-	if(n=='monospace'){fontLoaderSetFont(n);JVC.setFontFamily(n);return}
-	if(n=='jvc-default'){n=JVC.defFontFamily;fontLoaderSetFont(n);JVC.setFontFamily(n);return};
-	fontLoaderSetText();
-	var l='https://fonts.googleapis.com/css?family='+encodeURIComponent(n);
-	var d=$('<link rel=stylesheet class=font-loader-css>').on('load',()=>{fontLoaderWait(n)}).on('error',console.error);
-	d.attr('href',l).appendTo('head');fontLoaderSetFont(n);};
-
-$(function($){
-	fontLoaderNode();
-	var setStyle=(n)=>{JVC.setStyle(n)};
-	var setFontn=(n)=>{try{JVC_setFontn(n)}catch(e){setTimeout(JVC.setFontFamily,100,'"'+n+'"');console.error('setFontn',e)}};
-	var jvcStyle=page.getCookie('jvc-style','jvc-default');setStyle(jvcStyle);
-	var jvcFontn=page.getCookie('jvc-fontn','jvc-default');setFontn(jvcFontn);
+// MAIN
+(function($){
+	var jvcStyle=page.getCookie('jvc-style','jvc-default');
+	var jvcFontn=page.getCookie('jvc-fontn','jvc-default');
 	var icoDark=$('<span class="ico ico-sun">').on('click',page.dark);
 	var selStyle=$('<select>').append($("<option/>",{text:'no-style'}),$("<option/>",{text:'jvc-default',selected:true}));
 	var selFontn=$('<select>').append($("<option/>",{text:'monospace'}),$("<option/>",{text:'jvc-default',selected:true}));
@@ -55,18 +39,30 @@ $(function($){
 	var caption=$('title').text();if(caption=='jquery.json-viewer-callback')caption='JVC';
 	var title=$('<a class=title>').attr('href','https://github.com/andronick83/jquery.json-viewer-callback/').append(caption);
 	var navi=$('<div class="navi no-dark">');
-	navi.append(title,menu).prependTo('body');
 	//
-	setTimeout(()=>{
-		$.ajax('https://api.cdnjs.com/libraries/highlight.js?fields=assets').done(function(d){
-			var files=d.assets[0].files,opts=[],n;
-			for(i in files){n=files[i].match(/^styles\/(.+)\.min\.css$/);if(n)opts.push($("<option>",{text:n[1]}))}
-			selStyle.append(opts).val(jvcStyle);
-			selStyle.on('change',function(){var n=$(this).val();page.setCookie('jvc-style',n);setStyle(n)})})
-		$.ajax('https://cdn.jsdelivr.net/gh/herrstrietzel/fonthelpers@main/json/gfontsAPI.json').done(function(d){
-			var fonts=d.items,opts=[],n;
-			for(i in fonts){n=fonts[i];if(n.category=='monospace')opts.push($("<option>",{text:n.family}))}
-			selFontn.append(opts).val(jvcFontn);
-			selFontn.on('change',function(){var n=$(this).val();page.setCookie('jvc-fontn',n);setFontn(n)})})
-	},10)
-});
+	$(document).on('JVC:load',(v,JVC)=>{JVC.setStyle(jvcStyle)});
+	//
+	var JVC_callbackAjax=function(e,n,d,c){var ajax;
+		if(typeof d==='string')ajax={url:d,dataType:"json"};else ajax=d;
+		$.ajax(ajax).done(c).fail(function(xhr, status, err){c({"jvc-fail": status+' ('+(err? err :xhr.status)+')'})})};
+	//
+	document.addEventListener('DOMContentLoaded',function(){
+		var once=false;
+		JVC_setFontn(jvcFontn,(n)=>{
+			var $page=$('.page'),i;
+			navi.append(title,menu).prependTo('body');
+			$.ajax('https://api.cdnjs.com/libraries/highlight.js?fields=assets').done(function(d){
+				var files=d.assets[0].files,opts=[],n;
+				for(i in files){n=files[i].match(/^styles\/(.+)\.min\.css$/);if(n)opts.push($("<option>",{text:n[1]}))}
+				selStyle.append(opts).val(jvcStyle);
+				selStyle.on('change',function(){var n=$(this).val();page.setCookie('jvc-style',n);JVC.setStyle(n)})
+			});
+			$.ajax('https://cdn.jsdelivr.net/gh/herrstrietzel/fonthelpers@main/json/gfontsAPI.json').done(function(d){
+				var fonts=d.items,opts=[],n;
+				for(i in fonts){n=fonts[i];if(n.category=='monospace')opts.push($("<option>",{text:n.family}))}
+				selFontn.append(opts).val(jvcFontn);
+				selFontn.on('change',function(){var n=$(this).val();page.setCookie('jvc-fontn',n);JVC_setFontn(n)})
+			});
+		});
+	});
+})(jQuery);
